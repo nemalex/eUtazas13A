20451ef27e8fdf5722238ba3145e3718f3c09e5c
nemalex <nemalex14@gmail.com>
2020-10-06 09:01:18 +0200

    kiinduló állapot

--- src/Content.ts
+++ src/Content.ts
@@ -2,11 +2,6 @@
 import http from "http";
 import url from "url";
 
-// Függvény definiálása:
-function összead(a: number, b: number): number {
-    return a + b;
-}
-
 export default class Content {
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
         // favicon.ico kérés kiszolgálása:
@@ -30,71 +25,19 @@ export default class Content {
 
         // Kezd a kódolást innen -->
 
-        // Egyszerű üzenet megjelenítése a weboldalom
-        res.write("Próba\n");
-        res.write("Próba2\n");
+        res.write("Egyszerű Hello World!\n");
 
-        // Ez egy egysoros megjegyzés
-        /* Ez
-        egy
-        többsoros megjegyzés */
+        // Tetszőleges html teg-ek és attribútumok beépítése:
+        res.write("<span style='color: blue;'><i>Színes és dőlt Hello World!'</i></span>\n");
 
-        // var
-        // var message: string = "Hello World";
-        // Unexpected var, use let or const instead.
-        // res.write(message);
+        // Próbáljuk számra konvertálni a "kor" paraméter (http://localhost:8080/?kor=16) értékét:
+        let korod: number = parseInt(params.kor as string);
+        // Ha nincs "kor" paraméter megadva, vagy nem lehet számra konvertálni értékét,
+        // akkor a "korod" változóba NaN érték kerül, ilyenkor legyen 18 év az értéke:
+        if (isNaN(korod)) korod = 18;
 
-        // Változó típus nélkül (Any)
-        const a = 10;
-        res.write(`${a}\n`);
-
-        // Beépített típusok
-        const szám: number = 34.23424;
-        res.write(`${szám}\n`);
-
-        const logikai: boolean = true;
-        res.write(`${logikai}\n`);
-
-        const pii: number = 3.14;
-        res.write(`${pii === 3.14}\n`);
-
-        // typeof operátor
-        res.write(`${typeof szám}\n`);
-        res.write(`${typeof logikai}\n`);
-
-        // if
-        if (szám > 0) {
-            res.write("A szám pozitív!\n");
-        }
-
-        // for
-        const num: number = 5;
-        let factorial: number = 1;
-        let i: number = 2;
-        for (;;) {
-            factorial *= i;
-            i++;
-            if (i > num) {
-                break;
-            }
-        }
-        res.write(`${num}!=${factorial}\n`);
-
-        // for in - for of
-        const nevek: string[] = ["Alex", "Márk", "Bence", "Olga"];
-        // for in -> adatszerkezet index értékeit veszi fel i
-        for (const i in nevek) {
-            res.write(`${i}. -> ${nevek[i]}\n`);
-        }
-        // for of == c# foreach
-        for (const i of nevek) {
-            res.write(`${i}\n`);
-        }
-
-        // Függvény hívása
-        const op1: number = 5;
-        const op2: number = 20;
-        res.write(`${op1} + ${op2} = ${összead(op1, op2)}\n`);
+        res.write(`Kérem a korod: <input type='number' name='kor' value=${korod} style='max-width:100px;' onChange='this.form.submit();'>\n`);
+        res.write(`Te ${korod} éves vagy!\n`);
 
         // <---- Fejezd be a kódolást
 
